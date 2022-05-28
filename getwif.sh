7904c80bb255751e0e01177469bc721b74f6b4d4
BenWestgate <73506583+BenWestgate@users.noreply.github.com>
2022-05-28 09:45:29 -0500

    Update getwif.sh

--- getwif.sh
+++ getwif.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 ###################
 # Creates generator wallet, gets address, dumps its WIF key, leaves no traces behind.
-# Parameter $1 = command path for bitcoin-cli ie "bitcoin-cli" or 
-# "/snap/bitcoin-core/current/bin/bitcoin-cli". Tails usually needs '-rpcport=17600')
-# Call this script with "bash getwif.sh [Path to bitcoin-cli]"
+# Parameter $1 = command path for bitcoin-cli ie 'bitcoin-cli' or 
+# '/snap/bitcoin-core/current/bin/bitcoin-cli'. Tails usually needs '-rpcport=17600')
+# Call this script with 'bash getwif.sh [Path to bitcoin-cli]'
 # Note: bitcoind must already be running or 'Enable RPC Server' checked in GUI options and restart.
 ###################
 
