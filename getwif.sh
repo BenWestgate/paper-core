79a268231d937070c97f73a1dabbff130861b8c1
BenWestgate <73506583+BenWestgate@users.noreply.github.com>
2022-05-29 12:19:34 -0500

    Update getwif.sh

--- getwif.sh
+++ getwif.sh
@@ -11,7 +11,7 @@ temp=$(mktemp)	# creates temporary file, stores path in temp
 key=$(head -c64 /dev/urandom | base64)	# 64 byte wallet encryption key to crypto-shred the wallet
 $1 createwallet $temp false false "$key" false false >/dev/null
 $1 -rpcwallet=$temp walletpassphrase "$key" 1	# unlocks generator wallet for 1 second
-$1 -rpcwallet="$temp" dumpprivkey $($1 -rpcwallet="$temp" getnewaddress)
+$1 -rpcwallet=$temp dumpprivkey $($1 -rpcwallet=$temp getnewaddress)
 $1 -rpcwallet=$temp walletlock
 $1 -rpcwallet=$temp unloadwallet >/dev/null
 rm $temp
